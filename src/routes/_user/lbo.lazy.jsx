dd8364432f6b918b703f98ec9cee4b25ec40b099
SwymMhr <swoyammaharjan69@gmail.com>
2024-12-14 10:22:57 +0545

    Update lbo.lazy.jsx

--- src/routes/_user/lbo.lazy.jsx
+++ src/routes/_user/lbo.lazy.jsx
@@ -80,7 +80,7 @@ function RouteComponent() {
             type={capitalize(lbo.userField)}
             price={"Asked On: "+lbo.date}
             description={lbo.excerpt}
-            buttonText="View Request"
+            buttonText="Contact Now"
             link="https://api.whatsapp.com/send/?phone=9800000002&text&type=phone_number&app_absent=0"
           />
         ))
